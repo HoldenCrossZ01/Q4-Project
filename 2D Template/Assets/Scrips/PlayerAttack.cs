dabbecd4f7c9b223868b3f7b32943f0a1b750545
MUKILTEO\1722758 <1722758@mukilteo.wednet.edu>
2025-04-14 09:13:56 -0700

    Overriding Co-Programmers movement error, Z.

---
+++ 2D Template/Assets/Scrips/PlayerAttack.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}

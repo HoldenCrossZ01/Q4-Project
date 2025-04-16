1cc492040a65b9ddadee0e90ed0a0c23f4468a83
DESKTOP-G1NQUUG\User <1722758@mukilteo.wednet.edu>
2025-04-16 10:39:26 -0700

    Made some heavier progress into the beginning combat scripts, Z.

---
+++ 2D Template/Assets/Scrips/EnemyStats.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyStats : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+   
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- 2D Template/Assets/Scrips/PlayerAttack.cs
+++ 2D Template/Assets/Scrips/PlayerAttack.cs
@@ -8,14 +8,31 @@ public class PlayerAttack : MonoBehaviour
     public float startTimeBtwAttack;
 
     public Transform attackPos;
-    public float attackRange;
+    public int attackRange;
+    public int damage;
+
+    public LayerMask whatIsEnemies;
 
     // Update is called once per frame
     public void HandleAttack(InputAction.CallbackContext ctx)
     {
         if(timeBtwAttack <= 0)
         {
-          //Collider2D[] enemiesToDamage = Physics2D.OverlapAreaAll(attackPos.position, attackRange, 0,
+          Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, whatIsEnemies, attackRange);
+          for (int i = 0; i < enemiesToDamage.Length; i++) 
+          {
+            enemiesToDamage[i].GetComponent<EnemyStats>().health -= damage;
+          }
+          timeBtwAttack = startTimeBtwAttack;
         }
+        else 
+        {
+          timeBtwAttack -= Time.deltaTime;
+        }
+    }
+    void OnDrawGizmosSelected() 
+    {
+      Gizmos.color = Color.red;
+      Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 }

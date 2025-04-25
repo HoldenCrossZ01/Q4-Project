34fdada81bced1676c464c4b49f121b139458ef4
DESKTOP-G1NQUUG\User <1722758@mukilteo.wednet.edu>
2025-04-24 22:33:56 -0700

    .

---
+++ 2D Template/Assets/Scrips/CameraShake.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+//   public void Shake(float frequency, float amplitude, float time)
+//   {
+//     StartCoroutine(ShakeCoroutine(frequency, amplitude, time));
+//   }
+
+//    IEnumerator DamageOverTimeCoroutine(float frequency, float amplitude, float time)
+//    {
+//     var noise = GetComponent<CinemachineBasicMultiChannelPerlin>();
+
+//     noise.FrequencyGain = frequency;
+//     noise.AmplitudeGain = amplitude;
+
+//     yield return new WaitForSeconds(time);
+
+//     noise.FrequencyGain = 0;
+//     noise.AmplitudeGain = 0;
+//    }
+  
+
+}

2eb0caadfadb5426d74ab9e258f55434d0e974b3
MUKILTEO\1717545 <lphousego@gmail.com>
2025-05-05 08:13:58 -0700

    working on ai

--- 2D Template/Assets/AiChase.cs
+++ 2D Template/Assets/AiChase.cs
@@ -18,8 +18,10 @@ public class AiChase : MonoBehaviour
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         transform.position = Vector2.MoveTowards(this. transform.position, player.transform.position, speed * Time.deltaTime);
-
+//        transform.position = (Vector3.forward * angle);
     }
 }

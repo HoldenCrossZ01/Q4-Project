5eb95f7b6151fa98a0ca10deaaf3051be8a0465c
MUKILTEO\1702249 <1702249@mukilteo.wedmet.edu>
2025-05-05 10:14:08 -0700

    Credits

--- 2D Template/Assets/Secene.cs
+++ 2D Template/Assets/Secene.cs
@@ -7,4 +7,25 @@ public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void CreditLoader()
+    {
+        SceneManager.LoadScene("Laina");
+    }
+    public void CreditLoader2()
+    {
+        SceneManager.LoadScene("Coda");
+    }
+    public void CreditLoader3()
+    {
+        SceneManager.LoadScene("Thomas");
+    }
+    public void CreditLoader4()
+    {
+        SceneManager.LoadScene("Joshua");
+    }
+    public void CreditLoader5()
+    {
+        SceneManager.LoadScene("Meadow");
+    }
+
 }
